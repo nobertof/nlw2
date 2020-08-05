24cd24c6e5b64bc7ffc04e4bb00c95022e1acc17
Noberto filho <francisco.noberto.gomes@gmail.com>
2020-08-05 17:57:11 -0300

    conexão e finalização do backend e do front end

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-import './styles.css'
+import './styles.css';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number,
+    subject: string,
+    cost: number,
+    name: string,
+    avatar: string,
+    whatsapp: string,
+    bio: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher})=>{
+    function createNewConnection(){
+        api.post('connections',{user_id:teacher.id});
+    }
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/32619479?s=460&u=73445bb9ec11dd30e838bc7f9f2c7c8a6b0ab2d3&v=4" alt="Noberto filho"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Noberto filho</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                Entusiasta das melhores tecnologias de matemática avançada.
-                <br/><br/>
-                Apaixonado por calcular ate os proprios passos dados durante o dia
+                {teacher.bio}
             </p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
 
             </footer>
         </article>
